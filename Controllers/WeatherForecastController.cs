a98aa3a056ab6d28d49d3c89caf851a85fd9cde1
Jonathan Nagayoshi <jonathan@nagayoshi.com.br>
2020-04-24 23:44:50 -0300

    Added new Summary

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace sample_netcore_githubactions.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Ronaldo"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
